a7ec0637acd4cc591ad90355b0d8ee5394a7a7a1
Subashchandrabose <subashchandrabose@live.com>
2023-11-23 23:30:08 +0530

    Update models.py with Product model changes

--- models.py
+++ models.py
@@ -17,3 +17,13 @@ class Supplier(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=True)
     password = db.Column(db.String(60), nullable=True)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    category = db.Column(db.String(255), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    location = db.Column(db.String(255), nullable=False)
+    supplier_id = db.Column(db.Integer, db.ForeignKey('supplier.id'), nullable=False)
+    supplier = db.relationship('Supplier', backref=db.backref('products', lazy=True))
+    # Add other columns as needed
